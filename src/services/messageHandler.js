cc0747e845f616ea1a7aa8c70f8dd782c487f143
Jeremias Gonzalez <jeremiasgonzalez7464@gmail.com>
2025-02-25 16:46:59 -0300

    add saludo de bienvenida

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -5,15 +5,31 @@ const cleanPhoneNumber = (number) => {
 class MessageHandler {
   async handleIncomingMessage(message) {
     if (message?.type === "text") {
-      const response = `Echo: ${message.text.body}`;
-      await whatsappService.sendMessage(
-        cleanPhoneNumber(message.from),
-        response,
-        message.id
-      );
+      const incomingMessage = message.text.body.toLowerCase().trim();
+
+      if (this.isGreeting(incomingMessage)) {
+        await this.sendWelcomeMessage(cleanPhoneNumber(message.from), message.id);
+      } else {
+        const response = `Echo: ${message.text.body}`;
+        await whatsappService.sendMessage(
+          cleanPhoneNumber(message.from),
+          response,
+          message.id
+        );
+      }
       await whatsappService.markAsRead(message.id);
     }
   }
-}
+  isGreeting(message) {
+    const greetings = ["hola", "hello", "hi", "buenas tardes"];
+    return greetings.includes(message);
+  }
 
+  async sendWelcomeMessage(to, messageId) {
+    const welcomeMessage =
+      "Hola, Bienvenido a nuestro servicio de Panaderia online." +
+      "¿En Qué puedo ayudarte Hoy?";
+    await whatsappService.sendMessage(to, welcomeMessage, messageId);
+  }
+}
 export default new MessageHandler();
